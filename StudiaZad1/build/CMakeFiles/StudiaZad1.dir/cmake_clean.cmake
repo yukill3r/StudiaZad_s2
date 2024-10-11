8c49b2c7606d74064e3b43f11984d75250029673
yukiller <yukill3rforgames@gmail.com>
2024-10-11 13:13:57 +0200

    Cmake tests

---
+++ StudiaZad1/build/CMakeFiles/StudiaZad1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudiaZad1.dir/StudiaZad1.cpp.o"
+  "CMakeFiles/StudiaZad1.dir/StudiaZad1.cpp.o.d"
+  "StudiaZad1"
+  "StudiaZad1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StudiaZad1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
